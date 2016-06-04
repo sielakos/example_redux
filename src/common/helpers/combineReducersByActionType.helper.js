ccb1c387139aa4c9db3bd7172c40137731bb428e
sielakos <sielakos@gmail.com>
2016-06-04 09:14:09 +0200

    changed combine reducers to simpler form

--- src/common/helpers/combineReducersByActionType.helper.js
+++ src/common/helpers/combineReducersByActionType.helper.js
@@ -1,20 +1,16 @@
 /**
- * Small helper function that creates new reducer from reducersConfig array, which consist of {type, reducer} entries.
- * If action.type === entry.type then given reducer is run against state and action. 
- * Reducers are fired in order given in array, with state being result of reducers before fired against state and action.
- * Last state returned will be result of this newly create reducer.
+ * Small helper function that creates new reducer from reducersConfig object,
+ * with keys being action types and values reducers.
  *
  * @param reducersConfig
  * @returns {function()}
  */
 export default (reducersConfig) => {
   return (state, action) => {
-    return reducersConfig.reduce((state, {type, reducer}) => {
-      if (action.type === type) {
-        return reducer(state, action);
-      }
+    if (reducersConfig[action.type]) {
+      return reducersConfig[action.type](state, action);
+    }
 
-      return state;
-    }, state);
+    return state;
   };
 };
